6e6883d172aad4665f63e34afa90baa1161e33cc
Kevin Upton <k_upton@live.com>
2018-02-28 12:39:40 +1000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+var fs = require('fs');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'app.js',
+    libraryExport: ''
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loaders: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  externals: [nodeExternals()]
+};
